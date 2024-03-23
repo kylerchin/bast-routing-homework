35e169ffbc68b654e66749bc05b69a94fe670fc5
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2024-03-23 14:05:22 -0700

    base template for exercise 1

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+use std::error::Error;
+
+pub struct RoadNetwork {
+ // vertex id is an integer (i32)
+ // edge is tuple (i32, i32)
+}
+
+pub struct Edge {
+
+}
+
+impl RoadNetwork {
+    pub fn read_from_osm_file(path: &str) -> Result<RoadNetwork, Box<dyn Error>> {
+
+    }
+}
+
+#[cfg(tests)]
+mod tests {
+    #[test]
+    fn test_baden_wuerttemberg() {
+        let p 
+    }
+}
